5dddc1a0895dfff51ce874e7b6bac014cb2906ca
camilotrzn <camilotrzn@gmail.com>
2025-03-12 11:19:22 -0500

    Añade conexion tipo cliente con la API

---
+++ src/backend/scraper/news_api_client.py
@@ -0,0 +1,90 @@
+import requests
+from typing import List, Dict
+from src.config.settings import NEWS_API_KEY
+
+
+class newsAPIClient:
+
+    def __init__(self, api_key: str = NEWS_API_KEY):
+
+        self.api_key = NEWS_API_KEY
+        self.basicURL = "https://newsapi.org/v2"
+    
+
+    def get_top_headliners(self, country, category : str = None) -> List[Dict]:
+
+        endpoint = self.basicURL + "/top-headlines"
+
+        params = {
+            "country": country,
+            "apiKey": self.api_key,
+        }
+
+        if category:
+            params["category"] = category
+
+        response = requests.get(endpoint, params=params)
+        if response.status_code == 200:
+            return response.json().get("articles", [])
+        else:
+            print(f"Error obteniendo headliners: {response.status_code} - {response.text}")
+            return []
+
+    def get_by_keyword(self, keyword, sortBy, language, searchIn) -> List[Dict]: 
+
+        endpoint = self.basicURL + "/everything" 
+
+        params = {
+            "q": keyword,
+            "sortBy": sortBy,
+            "language":language,
+            "searchIn":searchIn,
+            "apiKey": self.api_key,
+        }
+
+        response = requests.get(endpoint, params=params)
+        if response.status_code == 200:
+            return response.json().get("articles", [])
+        else:
+            print(f"Error obteniendo resultados por palabras clave: {response.status_code} - {response.text}")
+            return []
+        
+    def get_for_source(self, source) -> List[Dict]:
+
+        endpoint =  self.basicURL + "/top-headlines"
+
+        params = {
+            "sources":source,
+            "apiKey":self.api_key,
+        }
+
+        response = requests.get(endpoint, params=params)
+        if response.status_code == 200:
+            return response.json().get("articles", [])
+        else:
+            print(f"Error obteniendo resultados por palabras clave: {response.status_code} - {response.text}")
+            return []
+
+
+
+if __name__ == "__main__":
+
+    client = newsAPIClient()
+
+    # headlines = client.get_top_headliners(country="us", category="entertainment")
+    # for article in headlines:
+    #      print(f"Title: {article['title']}")
+    #      print(f"URL: {article['url']}")
+    #      print()
+
+    # headlines = client.get_for_source(source="bbc-news")
+    # for article in headlines:
+    #     print(f"Title: {article['title']}")
+    #     print(f"URL: {article['url']}")
+    #     print()
+
+    everything = client.get_by_keyword(keyword ="Python", sortBy ="popularity", language = "en", searchIn = "title")
+    for article in everything:
+        print(f"Title: {article['title']}")
+        print(f"URL: {article['url']}")
+        print()
